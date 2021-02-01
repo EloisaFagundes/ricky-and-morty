e93f2ad9af074ca5d4061642a2717528686db87f
EloisaFagundes <lola.fernanda@hotmail.com>
2021-02-01 17:23:22 -0300

    chore: install storybook

---
+++ web/.storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../src/components/**/*.stories.tsx'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+};
